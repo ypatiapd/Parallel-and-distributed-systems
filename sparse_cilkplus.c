f2a49491e4b15bc1f3dc9529b88df82865dece1c
Ypatia Dami <ypatiapd@gmail.com>
2021-11-21 19:23:33 +0200

    Update sparse_cilkplus.c

--- sparse_cilkplus.c
+++ sparse_cilkplus.c
@@ -16,7 +16,6 @@ typedef struct {
     int start;
     int end;
     int nzv;
-    int M;
 }ThreadArgs;
 
 struct param{
@@ -37,7 +36,7 @@ int main(int argc, char *argv[])
     MM_typecode matcode;
     FILE *f;
     int M, N, nzv;
-    int i, *I, *J ;
+    int  *I, *J ;
 
     if ((f = fopen("com-Youtube.mtx", "r")) == NULL){
         printf("NULL pointer\n");
@@ -72,6 +71,7 @@ int main(int argc, char *argv[])
     I = (int *) malloc(2*nzv * sizeof(int));
     J = (int *) malloc(2*nzv * sizeof(int));
 
+    /*Store the rows at array I and columns at array J*/
     for (int i=0; i<nzv; i++)
     {
         fscanf(f, "%d %d\n", &J[i], &I[i]);
@@ -81,20 +81,21 @@ int main(int argc, char *argv[])
 
     fclose(f);
 
+    /*Convert from triagonal to symmetric quadratic*/
     for (int j=0; j<nzv; j++){
         I[nzv+j]=J[j];
         J[nzv+j]=I[j];
     }
 
+    /*Sort row values of I following CSR .J values follow the sorting of I*/
     quicksort(I,J,0,2*nzv-1);
 
     numOfThreads= atoi(argv[1]);
 
     struct param Args[numOfThreads];
 
-    printf("2xnzv=%d\n",2*nzv);
     gettimeofday (&startwtime, NULL);
-    int cut =2*nzv/numOfThreads;
+    int cut =2*nzv/numOfThreads;//size of sub array that the thread will work on
 
     for(int i=0;i<numOfThreads;i++){
         Args[i].args = (ThreadArgs*)malloc (sizeof (ThreadArgs));
@@ -103,7 +104,6 @@ int main(int argc, char *argv[])
         Args[i].args->start=i*cut;
         Args[i].args->end=(i+1)*cut;
         Args[i].args->nzv=nzv;
-        Args[i].args->M=M/numOfThreads;
     }
     Args[numOfThreads-1].args->end=2*nzv;
 
@@ -124,6 +124,7 @@ int main(int argc, char *argv[])
 
 }
 
+//commands for the basic function find_triangles at the sparse_serial file
 void *find_triangles(void *arg){
 
   ThreadArgs *Arg;
@@ -131,7 +132,6 @@ void *find_triangles(void *arg){
   int start=Arg->start;
   int end=Arg->end;
   int nzv=Arg->nzv;
-  int M=Arg->M;
   int z=start;
   int p=z;
   int q=0;
@@ -141,7 +141,6 @@ void *find_triangles(void *arg){
   int psteps=0;
   int sum=0;
   int triangles=0;
-  int same_row=0;
 
   while((Arg->A[p]==row_value)&&(p!=-1)){
       p--;
@@ -150,7 +149,6 @@ void *find_triangles(void *arg){
 
   while (z<end){
 
-      same_row=0;
       if((Arg->A[z]==row_value)&&(z!=start)){
           p-=psteps;
       }
